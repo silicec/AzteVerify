3743489bc53435e600e34760e547ffe24eace670
Vlad-Stefan <vladstefan2002@gmail.com>
2024-08-30 14:14:06 +0300

    added some credentials

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ module.exports = {
         // —— Things that are required for the whole project to work.
         token: "", // —— Bot token.
         botId: "", // —— Bot ID.
-        guildId: "", // —— Server id.
-        verifiedRole: "", // —— Aici e rolu verificat.
+        guildId: "1099254930075832330", // —— Server id.
+        verifiedRole: "1099255114549694605", // —— Aici e rolu verificat.
         
         // —— Set the bot's presence, for statusType see: https://discord-api-types.dev/api/discord-api-types-v10/enum/ActivityType
         statusType: 3, // 1 (STREAMING), 2 (LISTENING), 3 (WATCHING), 5 (COMPETING). Default is 0 (PLAYING). 
