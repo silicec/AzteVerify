7edcf8dbd2499504264e895daf8cec4f6e042446
Vlad-Stefan <vladstefan2002@gmail.com>
2024-08-31 17:11:00 +0300

    Bugfix

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    server: {
+        domain: "localhost",
+        https: false,
+        httpPort: 8080,
+    },
+
+    Discord: {
+        // —— Things that are required for the whole project to work.
+        token: "MTI3OTAzNjc4NzE3NTQ2MDg5NQ.GYIFMW.V4sjZ12xFONbSr1v8hNuNHl9hn0DPO7umiT0pQ", // —— Bot token.
+        botId: "1279036787175460895", // —— Bot ID.
+        guildId: "1099254930075832330", // —— Server id.
+        verifiedRole: "1099255114549694605", // —— Aici e rolu verificat.
+        
+        // —— Set the bot's presence, for statusType see: https://discord-api-types.dev/api/discord-api-types-v10/enum/ActivityType
+        statusType: 3, // 1 (STREAMING), 2 (LISTENING), 3 (WATCHING), 5 (COMPETING). Default is 0 (PLAYING). 
+        statusMsg: "Caut useri neverificati!",
+
+        rulesEnabled: false,
+
+    },
+
+    reCAPTCHA: {
+        secretKey: "",
+        publicKey: ""
+    }
+}
